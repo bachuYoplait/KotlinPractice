7c1e8117555c8e81e6856896780cd809878190f4
kimkwangmin <foxcats1234@gmail.com>
2021-01-06 23:17:52 +0900

    practice kotlin 2021-01-06

--- kotlinPractice/src/Fucntion/FunctionTest.kt
+++ kotlinPractice/src/Fucntion/FunctionTest.kt
@@ -1,5 +1,17 @@
 package Fucntion
 
-fun main(args: Array<String>) : Unit {
-    print("Happy new Year");
+fun main(args: Array<String>): Unit {
+    println("Happy new Year")
+    println(sum(1, 2))
+    println(sum2(1, 2))
+    println(sum3(1, 2))
+
 }
+
+fun sum(a: Int, b: Int): Int {
+
+    return a + b
+}
+
+fun sum2 (a:Int, b:Int) :Int=a+b
+fun sum3 (a:Int, b:Int) =a+b
